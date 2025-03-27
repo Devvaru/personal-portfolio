fd083b003f503f774c1008179b3d43a517683e49
Devvaru <syeraru@gmail.com>
2025-03-26 19:12:49 -0600

    Add nav placeholders

---
+++ portfolio/src/app/navigation.tsx
@@ -0,0 +1,11 @@
+export default function Navigation() {
+    return (
+        <main className={"nav-container"}>
+            <div className={"nav"}>
+                <button className={"nav-link"}>Projects</button>
+                <button className={"nav-link"}>Skills</button>
+                <button className={"nav-link"}>Web Design</button>
+            </div>
+        </main>
+    );
+}
