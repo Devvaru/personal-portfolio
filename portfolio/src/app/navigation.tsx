d89230fd650fcefd4eecf17c64d219915043c1e2
Devvaru <syeraru@gmail.com>
2025-03-28 18:30:18 -0600

    Add links and icons

--- portfolio/src/app/navigation.tsx
+++ portfolio/src/app/navigation.tsx
@@ -1,10 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 export default function Navigation() {
     return (
-        <main className={"nav-container"}>
-            <div className={"nav"}>
-                <button className={"nav-link"}>Projects</button>
-                <button className={"nav-link"}>Skills</button>
-                <button className={"nav-link"}>Web Design</button>
+        <main className={"nav-container h-fit bg-white p-4 flex justify-between"}>
+            <div className={"nav flex gap-2 text-xl"}>
+                <a href={"#projects"} className={"nav-link"}>Projects</a>
+                <a href={"#stacks"} className={"nav-link"}>Stacks</a>
+                <a href={""} className={"nav-link"}>Design</a>
+                <a href={"#contact"} className={"nav-link"}>Contact</a>
+            </div>
+            <div className="flex gap-2">
+                <a href="https://www.linkedin.com/in/devon-g-b627872a1/">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" className={"fa-brands"} />
+                </a>
+                <a href="https://github.com/Devvaru">
+                    <FontAwesomeIcon icon={faGithub} size="2x" className={"fa-brands"} />
+                </a>
             </div>
         </main>
     );
