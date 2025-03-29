8f7a7c7856b398a26db6e7a65f4114cf1e7273d1
Devvaru <syeraru@gmail.com>
2025-03-28 22:36:19 -0600

    Create and populate stacks component

--- portfolio/src/app/stacks.tsx
+++ portfolio/src/app/stacks.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+
 export default function Stacks() {
     return (
         <main>
-            
+            <section className="flex gap-5 overflow-hidden">
+                <Image src="/logos/javascript.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/typescript.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/reactjs.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/react-bootstrap.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/nextjs.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/postgresql.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/html5.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/css3.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/nodejs.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/git.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/sass.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/cypress.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/mocha.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/jest.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/wordpress.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/cloudflare.svg" alt="CSS3" width={100} height={100} />
+                <Image src="/logos/jquery.svg" alt="CSS3" width={100} height={100} />
+
+
+            </section>
         </main>
     )
 }
