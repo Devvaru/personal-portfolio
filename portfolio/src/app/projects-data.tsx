bb5f1c6c1be6e838fb96d8e27d5abd3af6533d8e
Devvaru <syeraru@gmail.com>
2025-04-01 22:43:18 -0600

    Update layout for project data

--- portfolio/src/app/projects-data.tsx
+++ portfolio/src/app/projects-data.tsx
@@ -3,31 +3,36 @@ const projects: Readonly<Record<string, Readonly<Record<string, string | Readonl
         title: "PawTrackr",
         url: "https://github.com/Devvaru/PawTrackr",
         imgs: ["/images/Homepage_View.png", "/images/Pet_Modal_View.png", "/images/Reminders_View.png"],
-        frameworks: ["react", "react-bootstrap", "node.js", "javascript", "sass", "postgresql", "axios"]
+        description: "Full stack web app that enables you to manage all your pet care needs. Schedule feeding time, create exercise reminders, and vet appointments. Includes a calendar, drag & drop, and pagination",
+        frameworks: ["React", "React-Bootstrap", "Node.js", "JavaScript", "Sass", "Postgresql", "Axios"]
     },
     project2: {
         title: "Photolabs",
         url: "https://github.com/Devvaru/Photolabs",
         imgs: ["/images/photolabs_home.png", "/images/photolabs_modal.png", "/images/photolabs_topic.png"],
-        frameworks: ["react", "express.js", "postgresql"]
+        description: "Photolabs is a React based web app where you can view and favourite the photos of photographers, as well as search photos by topic.",
+        frameworks: ["React", "Express.js", "PostgreSQL"]
     },
     project3: {
         title: "Resource Wall",
         url: "https://github.com/Devvaru/resource-wall",
         imgs: ["/images/homepage-screenshot.jpg", "/images/my-favourites-screenshot.jpg", "/images/show-single-resource-screenshot.jpg"],
-        frameworks: ["javascript", "jquery", "express.js", "ejs", "sass", "postgresql"]
+        description: "Resource Wall is a resource sharing site that allows users to save and share learning resources — such as tutorials, blogs and videos — in a central place, publicly available to any user.",
+        frameworks: ["JavaScript", "jQuery", "Express.js", "EJS", "Sass", "PostgreSQL"]
     },
     project4: {
         title: "TwO-O-Player-Math-Game",
         url: "https://github.com/Devvaru/TwO-O-Player-Math-Game",
         imgs: [""],
-        frameworks: ["ruby"]
+        description: "A 2-Player CLI based math game written in Ruby, where players take turns to answer simple math addition problems. A new math question is generated for each turn by picking two numbers between 1 and 20. The player whose turn it is is prompted the question and must answer correctly or lose a life.",
+        frameworks: ["Ruby"]
     },
     project5: {
         title: "Etch-A-Sketch",
         url: "https://github.com/Devvaru/Etch-a-Sketch",
         imgs: ["/images/apple.jpg", "/images/portrait.jpg", "/images/photo.jpg"],
-        frameworks: ["javascript", "html5", "css3"]
+        description: "Etch-a-Sketch is an online drawing canvas where you can use various tools to create beautiful pixel art, including a colour picker, a grayscale pencil, and a color randomizer. You also have the option to change canvas size to your liking.",
+        frameworks: ["JavaScript", "HTML5", "CSS3"]
     },
 };
 
--- portfolio/src/app/projects.tsx
+++ portfolio/src/app/projects.tsx
@@ -2,29 +2,52 @@ import projects from "./projects-data";
 
 export default function Projects() {
     return (
-        <main className="projects-container">
+        <main className="projects-container flex flex-col items-center gap-8 px-12 py-8">
             {Object.entries(projects).map(([key, project]) => (
-                <div key={key} className="project-card">
-                    <h3>{project.title}</h3>
-                    {typeof project.url === "string" && (
-                        <a href={project.url} target="_blank" rel="noopener noreferrer">
-                            {project.url}
-                        </a>
-                    )}
+                <div key={key} className="project-card border rounded-xl p-8 flex gap-8">
 
-                    <div className="project-images">
+                    {/* images for modal*/}
+                    {/* <div className="project-images">
                         {Array.isArray(project.imgs) && project.imgs.map((img, index) => (
                             img && <img key={index} src={img} alt={`${project.title} screenshot ${index + 1}`} style={{ width: 300 }} />
                         ))}
+                    </div> */}
+
+                    {/* image */}
+                    <div className="project-image max-w-1/2">
+                        <img src={project.imgs[0]} />
                     </div>
 
-                    {Array.isArray(project.frameworks) && (
-                        <ul className="frameworks-list">
-                            {project.frameworks.map((framework, index) => (
-                                <li key={index}>{framework}</li>
-                            ))}
-                        </ul>
-                    )}
+                    <div className="flex flex-col gap-4">
+
+                        {/* title */}
+                        {typeof project.url === "string" && (
+                            <a href={project.url} target="_blank" rel="noopener noreferrer">
+                                <h3 className="text-2xl text-cyan-950">{project.title}</h3>
+                            </a>
+                        )}
+
+                        {typeof project.description === "string" && (
+                            <p className="text-cyan-950">{project.description}</p>
+                        )}
+
+                        {/* stacks */}
+                        {Array.isArray(project.frameworks) && (
+                            <ul className="frameworks-list flex gap-2 text-cyan-800 font-bold"> {"|"}
+                                {project.frameworks.map((framework, index) => (
+                                    <li key={index}>{framework + " | "}</li>
+                                ))}
+                            </ul>
+                        )}
+
+                        {/* button */}
+                        {typeof project.url === "string" && (
+                            <a href={project.url} target="_blank" rel="noopener noreferrer" className="py-2 px-6 bg-cyan-600 hover:bg-cyan-800 text-white rounded-full w-40 flex justify-center font-semibold">
+                                View Project
+                            </a>
+                        )}
+
+                    </div>
                 </div>
             ))}
         </main>
