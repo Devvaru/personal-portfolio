0301bd6f20a6f66144513dacae1d4322f55fcef1
Devvaru <syeraru@gmail.com>
2025-04-08 23:36:20 -0600

    Remove dysfunctional duration

--- portfolio/src/app/navigation.tsx
+++ portfolio/src/app/navigation.tsx
@@ -6,10 +6,10 @@ export default function Navigation() {
     return (
         <main className={"nav-container h-fit bg-white px-20 py-4 flex justify-between"}>
             <div className={"nav flex gap-3 text-xl"}>
-                <a href={"#projects"} className={"nav-link hover:underline decoration-2 duration-300 text-cyan-950 font-semibold"}>Projects</a>
-                <a href={"#stacks"} className={"nav-link hover:underline decoration-2 duration-300 text-cyan-950 font-semibold"}>Stacks</a>
-                <a href={""} className={"nav-link hover:underline decoration-2 duration-300 text-cyan-950 font-semibold"}>Design</a>
-                <a href={"#contact"} className={"nav-link hover:underline decoration-2 duration-300 text-cyan-950 font-semibold"}>Contact</a>
+                <a href={"#projects"} className={"nav-link hover:underline decoration-2 text-cyan-950 font-semibold"}>Projects</a>
+                <a href={"#stacks"} className={"nav-link hover:underline decoration-2 text-cyan-950 font-semibold"}>Stacks</a>
+                <a href={""} className={"nav-link hover:underline decoration-2 text-cyan-950 font-semibold"}>Design</a>
+                <a href={"#contact"} className={"nav-link hover:underline decoration-2 text-cyan-950 font-semibold"}>Contact</a>
             </div>
             <div className="flex gap-3">
                 <a href="https://www.linkedin.com/in/devon-g-b627872a1/" target='_blank'>
