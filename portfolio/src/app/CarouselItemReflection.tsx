816b890e40986103fe0327378dcab14063027d4f
Devvaru <syeraru@gmail.com>
2025-04-16 17:12:54 -0600

    Fix reflection flipping upside down

--- portfolio/src/app/CarouselItemReflection.tsx
+++ portfolio/src/app/CarouselItemReflection.tsx
@@ -3,7 +3,7 @@ import { CardDetail } from "./types";
 
 export default function CarouselItemReflection({ imgUrl, imgTitle, imgAlt }: CardDetail) {
     return (
-        <div className="carousel-card carousel-reflection">
+        <div className="carousel-reflection">
             <img src={imgUrl} title={imgTitle} alt={imgAlt} ></img>
         </div>
     );
