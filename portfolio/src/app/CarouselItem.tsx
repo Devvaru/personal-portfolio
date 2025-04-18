a68f86e105ade5f0e0f711f3f29e3c092a83ca8c
Devvaru <syeraru@gmail.com>
2025-04-18 01:01:49 -0600

    Refactor carousel css to use tailwind except animations

--- portfolio/src/app/CarouselItem.tsx
+++ portfolio/src/app/CarouselItem.tsx
@@ -3,8 +3,8 @@ import { CardDetail } from "./types";
 
 export default function CarouselItem({ imgUrl, imgTitle, imgAlt }: CardDetail) {
     return (
-        <div className="carousel-card">
-            <img src={imgUrl} title={imgTitle} alt={imgAlt} ></img>
+        <div className="carousel-card flex flex-auto justify-center items-center h-full w-[75px] lg:w-[100px]">
+            <img src={imgUrl} title={imgTitle} alt={imgAlt} className="h-full transition-transform duration-300 hover:scale-90"></img>
         </div>
     );
 }
