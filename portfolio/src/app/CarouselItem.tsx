07bceec6374d9c2e9ab65bfe8a35130293e7aba6
Devvaru <syeraru@gmail.com>
2025-04-25 19:35:42 -0600

    Fix build issues

--- portfolio/src/app/CarouselItem.tsx
+++ portfolio/src/app/CarouselItem.tsx
@@ -4,7 +4,7 @@ import { CardDetail } from "./types";
 export default function CarouselItem({ imgUrl, imgTitle, imgAlt }: CardDetail) {
     return (
         <div className="carousel-card flex flex-auto justify-center items-center h-full w-[75px] lg:w-[100px]">
-            <img src={imgUrl} title={imgTitle} alt={imgAlt} className="h-full transition-transform duration-300 hover:scale-90"></img>
+            <img src={imgUrl} title={imgTitle} alt={imgAlt} className="h-full transition-transform duration-300 hover:scale-90" />
         </div>
     );
 }
