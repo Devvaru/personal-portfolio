48d6432601a7d2fd5605a246c3c0c4fb00cc9441
Devvaru <syeraru@gmail.com>
2025-04-25 19:07:47 -0600

    Update next config

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/personal-portfolio",
+  assetPrefix: "/personal-portfolio",
 };
 
 export default nextConfig;
