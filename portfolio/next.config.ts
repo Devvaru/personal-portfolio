080ed09bcdf8e980fb33d868143f56ef91f13cf8
Devvaru <syeraru@gmail.com>
2025-05-08 23:18:31 -0600

    Remove basepath variable

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: "/personal-portfolio",
-  assetPrefix: "/personal-portfolio",
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
--- portfolio/src/app/CarouselData.tsx
+++ portfolio/src/app/CarouselData.tsx
@@ -1,95 +1,94 @@
 import { CardDetail } from "./types";
-const basePath = "/personal-portfolio"; // for production
 
 export const cardDetails: Record<string, CardDetail> = {
     0: {
-        imgUrl: `${basePath}/logos/javascript.svg`,
+        imgUrl: `/logos/javascript.svg`,
         imgTitle: "JavaScript",
         imgAlt: "JavaScript Logo"
     },
 
     1: {
-        imgUrl: `${basePath}/logos/typescript.svg`,
+        imgUrl: `/logos/typescript.svg`,
         imgTitle: "TypeScript",
         imgAlt: "TypeScript Logo"
     },
 
     2: {
-        imgUrl: `${basePath}/logos/reactjs.svg`,
+        imgUrl: `/logos/reactjs.svg`,
         imgTitle: "React.js",
         imgAlt: "React Logo"
     },
 
     3: {
-        imgUrl: `${basePath}/logos/react-bootstrap.svg`,
+        imgUrl: `/logos/react-bootstrap.svg`,
         imgTitle: "React-Bootstrap",
         imgAlt: "React Bootstrap Logo"
     },
 
     4: {
-        imgUrl: `${basePath}/logos/nextjs.svg`,
+        imgUrl: `/logos/nextjs.svg`,
         imgTitle: "Next.js",
         imgAlt: "Next JS Logo"
     },
 
     5: {
-        imgUrl: `${basePath}/logos/postgresql.svg`,
+        imgUrl: `/logos/postgresql.svg`,
         imgTitle: "PostgreSQL",
         imgAlt: "PostgreSQL Logo"
     },
 
     6: {
-        imgUrl: `${basePath}/logos/html5.svg`,
+        imgUrl: `/logos/html5.svg`,
         imgTitle: "HTML5",
         imgAlt: "HTML5 Logo"
     },
     7: {
-        imgUrl: `${basePath}/logos/css3.svg`,
+        imgUrl: `/logos/css3.svg`,
         imgTitle: "CSS3",
         imgAlt: "CSS3 Logo"
     },
     8: {
-        imgUrl: `${basePath}/logos/nodejs.svg`,
+        imgUrl: `/logos/nodejs.svg`,
         imgTitle: "Node.js",
         imgAlt: "Node JS Logo"
     },
     9: {
-        imgUrl: `${basePath}/logos/git.svg`,
+        imgUrl: `/logos/git.svg`,
         imgTitle: "Git",
         imgAlt: "Git Logo"
     },
     10: {
-        imgUrl: `${basePath}/logos/sass.svg`,
+        imgUrl: `/logos/sass.svg`,
         imgTitle: "Sass",
         imgAlt: "Sass Logo"
     },
     11: {
-        imgUrl: `${basePath}/logos/cypress.svg`,
+        imgUrl: `/logos/cypress.svg`,
         imgTitle: "Cypress",
         imgAlt: "Cypress Logo"
     },
     12: {
-        imgUrl: `${basePath}/logos/mocha.svg`,
+        imgUrl: `/logos/mocha.svg`,
         imgTitle: "Mocha",
         imgAlt: "Mocha Logo"
     },
     13: {
-        imgUrl: `${basePath}/logos/jest.svg`,
+        imgUrl: `/logos/jest.svg`,
         imgTitle: "Jest",
         imgAlt: "Jest Logo"
     },
     14: {
-        imgUrl: `${basePath}/logos/wordpress.svg`,
+        imgUrl: `/logos/wordpress.svg`,
         imgTitle: "Wordpress",
         imgAlt: "Wordpress Logo"
     },
     15: {
-        imgUrl: `${basePath}/logos/cloudflare.svg`,
+        imgUrl: `/logos/cloudflare.svg`,
         imgTitle: "Cloudflare",
         imgAlt: "Cloudflare Logo"
     },
     16: {
-        imgUrl: `${basePath}/logos/jquery.svg`,
+        imgUrl: `/logos/jquery.svg`,
         imgTitle: "jQuery",
         imgAlt: "JQuery Logo"
     },
