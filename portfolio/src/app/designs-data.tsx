56936b6d0d8b1ab1f7a066a89d5292faf76c4d8d
Devvaru <syeraru@gmail.com>
2025-05-27 21:31:11 -0600

    Add designs and their data

---
+++ portfolio/src/app/designs-data.tsx
@@ -0,0 +1,39 @@
+const projects: Readonly<Record<string, Readonly<Record<string, string | ReadonlyArray<string>>>>> = {
+    design1: {
+        title: "Calculator",
+        img: `/images/Calculator - Design.jpg`,
+        description: "",
+    },
+    design2: {
+        title: "Resource Wall Home Page",
+        imgs: `/images/Resource Wall - LHL - Design.jpg`,
+        description: "",
+    },
+    design3: {
+        title: "Resource Wall Item Page",
+        imgs: `/images/Resource Wall - Resource Page - LHL - Design.jpg`,
+        description: "",
+    },
+    design4: {
+        title: "First Portfolio",
+        imgs: `/images/Portfolio - FreeCodeCamp - Design.jpg`,
+        description: "",
+    },
+    design5: {
+        title: "Etch-A-Sketch",
+        imgs: `/images/Etch-A-Sketch - Design.jpg`,
+        description: "",
+    },
+    design6: {
+        title: "Camping Form",
+        imgs: `/images/Camping Form - Design.jpg`,
+        description: "",
+    },
+    design7: {
+        title: "Coffee Store Landing Page",
+        imgs: `/images/Landing Page - FreeCodeCamp - Design.jpg`,
+        description: "",
+    },
+};
+
+export default projects;
